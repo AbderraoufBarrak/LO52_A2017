5006780bffdea2e022ce896fcd92ff6914c5286a
BRAVO-Y <yvan.bravo68@gmail.com>
2017-09-23 09:18:49 +0200

    Volant Adapter

--- Taaroa_eats_flammekueche/TP1/TaaroaFFBaD/app/src/main/java/fr/utbm/lo52/taaroaffbad/Activity/VolantActivity.java
+++ Taaroa_eats_flammekueche/TP1/TaaroaFFBaD/app/src/main/java/fr/utbm/lo52/taaroaffbad/Activity/VolantActivity.java
@@ -2,28 +2,31 @@ package fr.utbm.lo52.taaroaffbad.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import fr.utbm.lo52.taaroaffbad.Adapter.VolantAdapter;
+import fr.utbm.lo52.taaroaffbad.Beans.Volant;
 import fr.utbm.lo52.taaroaffbad.R;
 
 public class VolantActivity extends AppCompatActivity {
 
     public ListView listView;
+    ArrayList<Volant> volantList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volant);
 
-        String[] stringsArray = new String[30];
-        for (int i = 0; i < 30; ++i)
-            stringsArray[i] = Integer.toString(i);
-        listView = (ListView)findViewById(R.id.listVolant);
+        volantList.add(new Volant("2015-2016","206-2017","Toshibo","Test",2));
+        volantList.add(new Volant("2015-2016","206-2017","Blabla","aaaaa",1));
+        volantList.add(new Volant("2015-2016","206-2017","Micka","Test",3));
+        volantList.add(new Volant("2015-2016","206-2017","Yvan","aaaaa",1));
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(VolantActivity.this,
-                android.R.layout.simple_list_item_1, stringsArray);
-        listView.setAdapter(adapter);
+        listView = (ListView) findViewById(R.id.listVolant);
+        listView.setAdapter(new VolantAdapter(VolantActivity.this, volantList));
 
     }
 }
---
+++ Taaroa_eats_flammekueche/TP1/TaaroaFFBaD/app/src/main/java/fr/utbm/lo52/taaroaffbad/Adapter/VolantAdapter.java
@@ -0,0 +1,72 @@
+package fr.utbm.lo52.taaroaffbad.Adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import fr.utbm.lo52.taaroaffbad.Beans.Volant;
+import fr.utbm.lo52.taaroaffbad.R;
+
+/**
+ * Created by Moi on 22/09/2017.
+ */
+
+public class VolantAdapter extends BaseAdapter {
+
+    private Activity activity;
+    private LayoutInflater inflater;
+    private List<Volant> volantList;
+
+    public VolantAdapter(Activity activity, List<Volant> volantList) {
+        this.activity = activity;
+        this.volantList = volantList;
+    }
+
+    @Override
+    public int getCount() {
+        return volantList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return volantList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (inflater == null)
+            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null)
+            convertView = inflater.inflate(R.layout.adapter_volant, null);
+
+        // getting volant data for the row
+        Volant v = volantList.get(position);
+
+        Log.i("Yvan",v.getClassement()+"");
+
+        // Marque
+        TextView marque = (TextView) convertView.findViewById(R.id.tvVolantName);
+        marque.setText(v.getMarque());
+
+        // Rating
+        RatingBar note = (RatingBar) convertView.findViewById(R.id.ratingBar);
+        note.setRating(v.getClassement());
+
+
+        return convertView;
+    }
+}
