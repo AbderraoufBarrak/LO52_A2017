c1828d8dbafc4d251bbdc86d5196a95488e10d71
ClementUTBM <clement.bareth@gmail.com>
2017-09-25 15:13:04 +0200

    Ajout dbHelper à git

---
+++ ElleEstOuLaPoulette/TP1/FFBad/app/src/main/java/utbm/fr/ffbad/db/DbHelper.java
@@ -0,0 +1,64 @@
+package utbm.fr.ffbad.db;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Clément on 22/09/2017.
+ */
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "SHUTTLECOCK";
+
+    Context context;
+    public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        this.context = context;
+    }
+
+    public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        InputStream is = getClass().getClassLoader().getResourceAsStream("db/SHUTTLECOCK.SQL");
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader bfr = new BufferedReader(isr);
+        String query = "";
+        String line;
+        try {
+            while((line = bfr.readLine()) != null){
+                query+=line+"\n";
+            }
+            db.execSQL(query);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e){
+            Log.e("Erreur sql",e.toString());
+        }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db){
+
+    }
+}
