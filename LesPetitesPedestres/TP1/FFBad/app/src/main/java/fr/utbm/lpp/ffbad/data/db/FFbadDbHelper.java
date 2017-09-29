4058b99edfa32d4efbfc5720f406ab0b0ad05c0a
lo52_LesPetitesPedestres <lo52@utbm.fr>
2017-09-29 11:49:55 +0200

    autofill db

--- LesPetitesPedestres/TP1/FFBad/app/src/main/java/fr/utbm/lpp/ffbad/data/db/FFbadDbHelper.java
+++ LesPetitesPedestres/TP1/FFBad/app/src/main/java/fr/utbm/lpp/ffbad/data/db/FFbadDbHelper.java
@@ -1,5 +1,6 @@
 package fr.utbm.lpp.ffbad.data.db;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -30,7 +31,13 @@ public class FFbadDbHelper extends SQLiteOpenHelper {
         db.execSQL(FFBadDbContract.Customer.SQL_CREATE_ENTRIES);
         db.execSQL(FFBadDbContract.Sale.SQL_CREATE_ENTRIES);
         db.execSQL(FFBadDbContract.Purchase.SQL_CREATE_ENTRIES);
+
+        createShuttlecock(db, "Yonex", "AS30", 500, 27);
+        createShuttlecock(db, "RSL", "Grade 3", 5000, 16.70);
+        createShuttlecock(db, "RSL", "Grade A9", 10000, 13.70);
+        createShuttlecock(db, "RSL", "Grade A1", 6000, 21);
     }
+
 
     private void deleteEntries(SQLiteDatabase db) {
         db.execSQL(FFBadDbContract.Shuttlecock.SQL_DELETE_ENTRIES);
@@ -38,5 +45,17 @@ public class FFbadDbHelper extends SQLiteOpenHelper {
         db.execSQL(FFBadDbContract.Customer.SQL_DELETE_ENTRIES);
         db.execSQL(FFBadDbContract.Sale.SQL_DELETE_ENTRIES);
         db.execSQL(FFBadDbContract.Purchase.SQL_DELETE_ENTRIES);
+    }
+
+    private long createShuttlecock (SQLiteDatabase db, String brand, String ref, int stock, double price) {
+        ContentValues values = new ContentValues();
+
+        values.put(FFBadDbContract.Shuttlecock.COLUMN_NAME_BRAND, brand);
+        values.put(FFBadDbContract.Shuttlecock.COLUMN_NAME_REFERENCE, ref);
+        values.put(FFBadDbContract.Shuttlecock.COLUMN_NAME_STOCK, stock);
+        values.put(FFBadDbContract.Shuttlecock.COLUMN_NAME_PRICE, price);
+
+        long rowID = db.insert(FFBadDbContract.Shuttlecock.TABLE_NAME, null, values);
+        return rowID;
     }
 }
