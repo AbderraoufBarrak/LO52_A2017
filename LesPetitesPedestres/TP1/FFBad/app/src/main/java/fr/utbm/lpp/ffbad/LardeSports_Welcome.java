9b92df26df57234f1a7b165b4bb19562ab2f07ec
lo52_LesPetitesPedestres <lo52@utbm.fr>
2017-09-29 10:48:29 +0200

    Rename MainActivity + creation of StockActivity

--- LesPetitesPedestres/TP1/FFBad/app/src/main/java/fr/utbm/lpp/ffbad/LardeSports_Welcome.java
+++ LesPetitesPedestres/TP1/FFBad/app/src/main/java/fr/utbm/lpp/ffbad/LardeSports_Welcome.java
@@ -3,7 +3,7 @@ package fr.utbm.lpp.ffbad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LardeSports_Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
