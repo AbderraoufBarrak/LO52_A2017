ad4318347a31363621e5b7fc2b00283c5f09b1c1
Florian Staine <florian.staine@utbm.fr>
2017-09-30 18:24:10 +0200

    Add entries into Customer and sales

--- LesPetitesPedestres/TP1/FFBad/app/src/main/java/fr/utbm/lpp/ffbad/data/db/FFbadDbHelper.java
+++ LesPetitesPedestres/TP1/FFBad/app/src/main/java/fr/utbm/lpp/ffbad/data/db/FFbadDbHelper.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class FFbadDbHelper extends SQLiteOpenHelper {
 
@@ -20,12 +21,18 @@ public class FFbadDbHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
         deleteEntries(sqLiteDatabase);
         onCreate(sqLiteDatabase);
     }
 
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
     private void createEntries(SQLiteDatabase db) {
+        Log.i("DbHelper", "Recreate entries");
         db.execSQL(FFBadDbContract.Shuttlecock.SQL_CREATE_ENTRIES);
         db.execSQL(FFBadDbContract.Trader.SQL_CREATE_ENTRIES);
         db.execSQL(FFBadDbContract.Customer.SQL_CREATE_ENTRIES);
@@ -36,6 +43,12 @@ public class FFbadDbHelper extends SQLiteOpenHelper {
         createShuttlecock(db, "RSL", "Grade 3", 5000, 16.70);
         createShuttlecock(db, "RSL", "Grade A9", 10000, 13.70);
         createShuttlecock(db, "RSL", "Grade A1", 6000, 21);
+
+        createCustomer(db, "FFBad Belfort", 1);
+        createCustomer(db, "Florian Staine", 2);
+
+        createSale(db, 1, 2, 3.50, 4, true);
+        createSale(db, 2, 2, 4.50, 4, false);
     }
 
     private void deleteEntries(SQLiteDatabase db) {
@@ -56,5 +69,26 @@ public class FFbadDbHelper extends SQLiteOpenHelper {
 
         long rowID = db.insert(FFBadDbContract.Shuttlecock.TABLE_NAME, null, values);
         return rowID;
+    }
+
+    private long createCustomer(SQLiteDatabase db, String name, int type) {
+        ContentValues values = new ContentValues();
+
+        values.put(FFBadDbContract.Customer.COLUMN_NAME_NAME, name);
+        values.put(FFBadDbContract.Customer.COLUMN_NAME_NAME, type);
+
+        return db.insert(FFBadDbContract.Customer.TABLE_NAME, null, values);
+    }
+
+    private long createSale(SQLiteDatabase db, int customer_id, int shuttlecock_id, double price, int quantity, boolean is_paid) {
+        ContentValues values = new ContentValues();
+
+        values.put(FFBadDbContract.Sale.COLUMN_NAME_CUSTOMER, customer_id);
+        values.put(FFBadDbContract.Sale.COLUMN_NAME_SHUTTLECOCK, shuttlecock_id);
+        values.put(FFBadDbContract.Sale.COLUMN_NAME_PRICE, price);
+        values.put(FFBadDbContract.Sale.COLUMN_NAME_QUANTITY, quantity);
+        values.put(FFBadDbContract.Sale.COLUMN_NAME_IS_PAID, is_paid);
+
+        return db.insert(FFBadDbContract.Sale.TABLE_NAME, null, values);
     }
 }
