730ec4f631441590521b72c2a0ce6381fc48a9b7
HuangMichel <michelhuang6@gmail.com>
2017-10-08 21:52:28 +0200

    ajout activite achat

---
+++ Mi/TP2/ShuttlesMgmt/app/src/main/java/com/example/shuttlesmgmt/ListViewAchat.java
@@ -0,0 +1,13 @@
+package com.example.shuttlesmgmt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListViewAchat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview_achat);
+    }
+}
