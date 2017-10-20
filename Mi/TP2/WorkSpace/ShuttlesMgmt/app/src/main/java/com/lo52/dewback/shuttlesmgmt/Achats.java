0c785b3ed7afd3536fcd37fc67615ebc3386186e
Toto <rometpierre@gmail.com>
2017-10-20 11:03:15 +0200

    Create TP2 project "ShuttlesMgmt" with "Achat" & "Stock"

---
+++ Mi/TP2/WorkSpace/ShuttlesMgmt/app/src/main/java/com/lo52/dewback/shuttlesmgmt/Achats.java
@@ -0,0 +1,13 @@
+package com.lo52.dewback.shuttlesmgmt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Achats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achats);
+    }
+}
