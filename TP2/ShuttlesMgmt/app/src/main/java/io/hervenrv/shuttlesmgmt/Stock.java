3af6825f63ea4211e6c480a90e4841704538d389
Basile BRENON <basile@bender>
2017-10-20 13:01:56 +0200

    Création du layout de cellule

--- TP2/ShuttlesMgmt/app/src/main/java/io/hervenrv/shuttlesmgmt/Stock.java
+++ TP2/ShuttlesMgmt/app/src/main/java/io/hervenrv/shuttlesmgmt/Stock.java
@@ -2,6 +2,10 @@ package io.hervenrv.shuttlesmgmt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Stock extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class Stock extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock);
+
+        ArrayList<String> text = new ArrayList<>();
+        text.add("Yonex - AS30 - Stock de 500 - 27 euros le tube");
+        text.add("RSL - Grade 3 - Stock de 5000 - 16,70 euros le tube");
+        text.add("RSL - Grade A9 - Stock de 10000 - 13.70 euros le tube");
+        text.add("RSL - Grade A1  - Stock de 6000 - 21 euros le tube");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_activated_1, text);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
     }
 }
