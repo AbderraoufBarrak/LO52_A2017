4485e078003cfe8c460225e7b8fd743665750449
lo52_bishop <lo52@utbm.fr>
2017-10-20 17:29:54 +0200

    Commit du TP1

---
+++ bishop/TP1/LO52-TP1/app/src/main/java/com/lo52/flo/lo52_tp1/DisplayActivity.java
@@ -0,0 +1,23 @@
+package com.lo52.flo.lo52_tp1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
