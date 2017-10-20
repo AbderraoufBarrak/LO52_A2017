957d1ee1292c5ff956b933d6cc4e6cf3e3a5e4df
Toto <rometpierre@gmail.com>
2017-10-20 12:28:09 +0200

    add source code: 4.1 tp2

--- Mi/TP2/WorkSpace/ShuttlesMgmt/app/src/main/java/com/lo52/dewback/shuttlesmgmt/MainActivity.java
+++ Mi/TP2/WorkSpace/ShuttlesMgmt/app/src/main/java/com/lo52/dewback/shuttlesmgmt/MainActivity.java
@@ -1,13 +1,27 @@
 package com.lo52.dewback.shuttlesmgmt;
-
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.lo52.dewback.shuttlesmgmt.stock_activity.Stock;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void AchatSend(View view) {
+        Intent intent = new Intent(this, Achats.class);
+        startActivity(intent);
+    }
+
+    public void StockSend(View view){
+        Intent intent = new Intent(this, Stock.class);
+        startActivity(intent);
     }
 }
