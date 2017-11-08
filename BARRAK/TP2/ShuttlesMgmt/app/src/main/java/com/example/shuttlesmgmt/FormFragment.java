209b5dad20c4869962afa91188c047373542377b
lo52_barrak <lo52@utbm.fr>
2017-11-09 00:23:56 +0100

    Commit Project TP2 Inclure le tri alphabétique de la liste déroulante du choix du tube souhaité

--- BARRAK/TP2/ShuttlesMgmt/app/src/main/java/com/example/shuttlesmgmt/FormFragment.java
+++ BARRAK/TP2/ShuttlesMgmt/app/src/main/java/com/example/shuttlesmgmt/FormFragment.java
@@ -10,6 +10,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class FormFragment extends Fragment {
@@ -19,6 +24,22 @@ public class FormFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         /*super.onCreateView(inflater,container,savedInstanceState);*/
         View view = inflater.inflate(R.layout.fragment_sign, container, false);
+
+        ArrayList<String> SourceArray = new ArrayList<String>();
+
+        Spinner Sourcespinner = (Spinner)view.findViewById(R.id.SpinnerFeedbackType);
+
+        ArrayAdapter<String> SourceArrayAdapter = new ArrayAdapter<String>(getActivity(),R.layout.simple_row_spinner,SourceArray);
+
+        SourceArrayAdapter.add("Chennai");
+        SourceArrayAdapter.add("Mumbai");
+        SourceArrayAdapter.add("Kolkatta");
+        SourceArrayAdapter.add("Delhi");
+
+        Sourcespinner.setAdapter(SourceArrayAdapter);
+
+        Collections.sort(SourceArray);
+
 
         return view;
     }
