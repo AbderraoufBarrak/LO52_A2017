c39e36afd136feff0f14d96eddfbf109121a97c1
nyanloutre <paul@nyanlout.re>
2017-11-14 21:00:05 +0100

    Correction refactoring variable URI_CHEESE

--- PersistenceContentProviderSample/app/src/main/java/com/example/android/contentprovidersample/AllHistorique.java
+++ PersistenceContentProviderSample/app/src/main/java/com/example/android/contentprovidersample/AllHistorique.java
@@ -43,7 +43,7 @@ public class AllHistorique extends AppCompatActivity {
                     switch (id) {
                         case LOADER_CHEESES:
                             return new CursorLoader(getApplicationContext(),
-                                    SampleContentProvider.URI_CHEESE,
+                                    SampleContentProvider.URI_VOLANT,
                                     new String[]{Volant.COLUMN_NAME},
                                     null, null, null);
                         default:
