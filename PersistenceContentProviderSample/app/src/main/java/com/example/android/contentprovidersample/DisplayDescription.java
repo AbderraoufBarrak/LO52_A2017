6820e7eededee2d0e1e8da81a6604d294b74a673
nyanloutre <paul@nyanlout.re>
2017-11-14 20:36:33 +0100

    Affichage de la date de la dernière vente

--- PersistenceContentProviderSample/app/src/main/java/com/example/android/contentprovidersample/DisplayDescription.java
+++ PersistenceContentProviderSample/app/src/main/java/com/example/android/contentprovidersample/DisplayDescription.java
@@ -9,12 +9,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.android.contentprovidersample.data.Converters;
+import com.example.android.contentprovidersample.data.Historique;
 import com.example.android.contentprovidersample.data.Volant;
 import com.example.android.contentprovidersample.provider.SampleContentProvider;
 
+import java.util.Date;
+
 public class DisplayDescription extends AppCompatActivity {
 
-    private static final int LOADER_CHEESES = 1;
+    private static final int LOADER_VOLANTS = 1;
+    private static final int LOADER_HISTORIQUE = 2;
     private Cursor mCursor;
     private int item_row;
 
@@ -24,10 +29,15 @@ public class DisplayDescription extends AppCompatActivity {
                 @Override
                 public Loader<Cursor> onCreateLoader(int id, Bundle args) {
                     switch (id) {
-                        case LOADER_CHEESES:
+                        case LOADER_VOLANTS:
                             return new CursorLoader(getApplicationContext(),
                                     SampleContentProvider.URI_VOLANT,
                                     new String[]{Volant.COLUMN_NAME},
+                                    null, null, null);
+                        case LOADER_HISTORIQUE:
+                            return new CursorLoader(getApplicationContext(),
+                                    SampleContentProvider.URI_HISTORIQUE,
+                                    new String[]{Historique.COLUMN_ID},
                                     null, null, null);
                         default:
                             throw new IllegalArgumentException();
@@ -37,11 +47,18 @@ public class DisplayDescription extends AppCompatActivity {
                 @Override
                 public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
                     switch (loader.getId()) {
-                        case LOADER_CHEESES:
+                        case LOADER_VOLANTS:
                             mCursor = data;
                             mCursor.move(item_row);
                             TextView mText = findViewById(R.id.nomVolant);
                             mText.setText(mCursor.getString(mCursor.getColumnIndexOrThrow(Volant.COLUMN_NAME)));
+                            break;
+                        case LOADER_HISTORIQUE:
+                            mCursor = data;
+                            mCursor.move(item_row);
+                            TextView mDerniereVente = findViewById(R.id.derniereVente);
+                            Date mDate = Converters.fromTimestamp(mCursor.getLong(mCursor.getColumnIndexOrThrow(Historique.COLUMN_DATE)));
+                            mDerniereVente.setText(mDate.toString());
                             break;
                     }
                 }
@@ -49,7 +66,10 @@ public class DisplayDescription extends AppCompatActivity {
                 @Override
                 public void onLoaderReset(Loader<Cursor> loader) {
                     switch (loader.getId()) {
-                        case LOADER_CHEESES:
+                        case LOADER_VOLANTS:
+                            mCursor = null;
+                            break;
+                        case LOADER_HISTORIQUE:
                             mCursor = null;
                             break;
                     }
@@ -65,6 +85,7 @@ public class DisplayDescription extends AppCompatActivity {
         Intent intent = getIntent();
         item_row = intent.getIntExtra("ITEM_ROW", 0);
 
-        getSupportLoaderManager().initLoader(LOADER_CHEESES, null, mLoaderCallbacks);
+        getSupportLoaderManager().initLoader(LOADER_VOLANTS, null, mLoaderCallbacks);
+        getSupportLoaderManager().initLoader(LOADER_HISTORIQUE, null, mLoaderCallbacks);
     }
 }
