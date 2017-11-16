bfc470cc4b91060760d5a837084b6b516b6a1170
Toto <rometpierre@gmail.com>
2017-11-16 21:29:06 +0100

    Fix bug flowwing merge between toto and notmo.
    Rename varible row_item to stock_row_item

--- Mi/TP2/WorkSpace/ShuttlesMgmt/app/src/main/java/com/lo52/dewback/shuttlesmgmt/stock_activity/view/CustomAdapter.java
+++ Mi/TP2/WorkSpace/ShuttlesMgmt/app/src/main/java/com/lo52/dewback/shuttlesmgmt/stock_activity/view/CustomAdapter.java
@@ -33,7 +33,7 @@ public class CustomAdapter extends ArrayAdapter<StockDataBean> implements View.O
     }
 
     public CustomAdapter(List<StockDataBean> data, Context context) {
-        super(context, R.layout.row_item, data);
+        super(context, R.layout.stock_row_item, data);
         this.dataSet = new ArrayList<>(data);
         this.mContext=context;
     }
@@ -64,7 +64,6 @@ public class CustomAdapter extends ArrayAdapter<StockDataBean> implements View.O
 
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.row_item, parent, false);
             viewHolder.brandTxtView = (TextView) convertView.findViewById(R.id.brand);
             viewHolder.nameTxtView = (TextView) convertView.findViewById(R.id.name);
             viewHolder.priceTxtView = (TextView) convertView.findViewById(R.id.price);
