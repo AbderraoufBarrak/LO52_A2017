83adaa6d6f5987375b7f3c97d906dd461249aa92
nyanloutre <paul@nyanlout.re>
2017-12-01 11:18:47 +0100

    Définition utilisateur dans le script Docker

--- TP3/run.sh
+++ TP3/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t ubuntu-lo52 .
-docker run -it -v $PWD/hikey-linaro:/mnt/hikey-linaro ubuntu-lo52
+docker run -it -v $PWD/hikey-linaro:/mnt/hikey-linaro --user $(id -u):$(id -g) ubuntu-lo52
