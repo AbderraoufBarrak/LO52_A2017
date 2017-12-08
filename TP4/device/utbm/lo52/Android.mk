ee561415b847cf6f4b0ff182f33c69831c053cfb
Mickael Constanzer <mickael.constanzer@laposte.net>
2017-12-08 12:28:40 +0100

    TP4 - Part2

---
+++ TP4/device/utbm/lo52/Android.mk
@@ -0,0 +1,3 @@
+overlay /frameworks/base/master/core/res/res/drawable-en-hdpi/sym_keyboard_delete.png
+overlay /frameworks/base/master/core/res/res/drawable-en-ldpi/sym_keyboard_delete.png
+overlay /frameworks/base/master/core/res/res/drawable-en-mdpi/sym_keyboard_delete.png
---
+++ TP4/device/utbm/lo52/lo52.mk
@@ -0,0 +1,12 @@
+$(call inherit_product, device/linaro/hikey/hikey.mk)
+PRODUCT_PACKAGES += libusb
+PRODUCT_PACKAGE_OVERLAYS := device/utbm/lo52/overlay
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro_hw = lo52 \
+	net.dns1 = 8.8.8.8 \
+	net.dns2 = 4.4.4.4			
+PRODUCT_NAME := lo52
+PRODUCT_DEVICE := lo52
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := lo52_eDashboard
+include $(call all-subdir-makefiles)
