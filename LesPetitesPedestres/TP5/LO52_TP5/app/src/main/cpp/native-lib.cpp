64cb67a43f2fccd1e15e6862f35550f1a3d6f680
Zilliq11 <zilliqman@gmail.com>
2017-12-15 10:47:28 +0100

    TP5 Application working

--- LesPetitesPedestres/TP5/LO52_TP5/app/src/main/cpp/native-lib.cpp
+++ LesPetitesPedestres/TP5/LO52_TP5/app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,7 @@
 #include <jni.h>
 #include <string>
+#include <string.h>
+#include <stdlib.h>
 
 extern "C"
 JNIEXPORT jstring JNICALL
@@ -8,4 +10,56 @@ Java_fr_utbm_lpp_lo52_1tp5_LO52MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+
+
+
+JNIEXPORT jstring JNICALL
+Java_fr_utbm_lpp_lo52_1tp5_LO52MainActivity_read(
+        JNIEnv* env,
+jobject /* this */, jstring x, jstring y) {
+    const char *nativeX = env->GetStringUTFChars(x, 0);
+    const char *nativeY = env->GetStringUTFChars(y, 0);
+    char buff[100];
+    strcpy(buff, nativeX);
+    strcat(buff, nativeY);
+    return env->NewStringUTF(buff);
+}
+
+JNIEXPORT jstring JNICALL
+Java_fr_utbm_lpp_lo52_1tp5_LO52MainActivity_write(
+        JNIEnv* env,
+        jobject /* this */, jstring x) {
+    const char *name = env->GetStringUTFChars(x, 0);
+    char buff[100];
+    strcpy(buff, "Hello ");
+    strcat(buff, name);
+    strcat(buff, " from C++");
+
+    return env->NewStringUTF(buff);
+}
+
+JNIEXPORT jint JNICALL
+Java_fr_utbm_lpp_lo52_1tp5_LO52MainActivity_start(
+        JNIEnv* env,
+        jobject /* this */, jint jni_i) {
+    jni_i *= jni_i;
+    return jni_i;
+}
+
+JNIEXPORT jint JNICALL
+Java_fr_utbm_lpp_lo52_1tp5_LO52MainActivity_stop(
+        JNIEnv* env,
+        jobject /* this */, jint jni_i) {
+    jni_i *= jni_i;
+    return jni_i;
+}
+
+JNIEXPORT jstring JNICALL
+Java_fr_utbm_lpp_lo52_1tp5_LO52MainActivity_reset(
+        JNIEnv* env,
+        jobject /* this */) {
+
+    return env->NewStringUTF(strerror(rand()%20));
 }
--- LesPetitesPedestres/TP5/LO52_TP5/app/src/main/java/fr/utbm/lpp/lo52_tp5/LO52MainActivity.java
+++ LesPetitesPedestres/TP5/LO52_TP5/app/src/main/java/fr/utbm/lpp/lo52_tp5/LO52MainActivity.java
@@ -2,18 +2,72 @@ package fr.utbm.lpp.lo52_tp5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import java.util.Random;
 
-public class LO52MainActivity extends AppCompatActivity {
 
+import org.w3c.dom.Text;
+
+public class LO52MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView label;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lo52_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        TextView champ = (TextView) findViewById(R.id.champssaisie);
+        label = (TextView) findViewById(R.id.label);
+        Button btnread = (Button) findViewById(R.id.btnread);
+        Button btnwrite = (Button) findViewById(R.id.btnwrite);
+        Button btnstart = (Button) findViewById(R.id.btnstart);
+        Button btnstop = (Button) findViewById(R.id.btnstop);
+        Button btnreset = (Button) findViewById(R.id.btnreset);
+
+        btnread.setOnClickListener(this);
+        btnwrite.setOnClickListener(this);
+        btnstart.setOnClickListener(this);
+        btnstop.setOnClickListener(this);
+        btnreset.setOnClickListener(this);
+
+        tv.setText(stringFromJNI());
+    }
+
+    @Override
+    public void onClick(View view) {
+        String toDisplay;
+        Random rand = new Random();
+
+        Log.d("bonjour", "avant switch");
+        switch (view.getId()){
+            case R.id.btnread :
+                Log.d("bonjour", "Bouton");
+                toDisplay = read("Salut", "Bonsoir");
+                label.setText("Read : " + toDisplay);
+                Log.i("bonjour", "Fin Bouton");
+                break;
+            case R.id.btnwrite :
+                toDisplay = write("Papa Lama");
+                label.setText("Write : " + toDisplay);
+                break;
+            case R.id.btnstart :
+                int i = 11 + rand.nextInt()%6;
+                label.setText(i + "/" + start(i));
+                break;
+            case R.id.btnstop :
+                int j = 1 + rand.nextInt()%10;
+                label.setText(j + "/" + stop(j));
+                break;
+            case R.id.btnreset :
+                label.setText("");
+                label.setText("Hey, le return code is : " + reset() + " ty for your attention." + "\nYou're a llama. Gracefully, Your D(e)ad Lama.");
+                break;
+
+        }
     }
 
     /**
@@ -21,6 +75,12 @@ public class LO52MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    public native String read(String x, String y);
+    public native String write(String x);
+    public native int start(int j);
+    public native int stop(int j);
+    public native String reset();
 
     // Used to load the 'native-lib' library on application startup.
     static {
