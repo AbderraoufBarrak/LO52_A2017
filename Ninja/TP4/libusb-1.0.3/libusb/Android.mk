5ad0efc610826b04d7762c24717a2c91e3ecc1f5
LO52-Marcel <32192287+LO52-Marcel@users.noreply.github.com>
2017-12-18 19:40:16 +0100

    Update Android.mk

--- Ninja/TP4/libusb-1.0.3/libusb/Android.mk
+++ Ninja/TP4/libusb-1.0.3/libusb/Android.mk
@@ -1,7 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE:=libusb_lib
-LOCAL_MODULE_TAGS:=optional
-LOCAL_SRC_FILES:=core.c descriptor.c io.c sync.c os/linux_usbfs.c
-LOCAL_C_INCLUDES+=libusb-1.0.3/libusb/os libusb-1.0.3/libusb/
+LOCAL_SRC_FILES := core.c \
+                   descriptor.c \
+                   io.c \ 
+                   sync.c \
+LOCAL_C_INCLUDES += libusb \
+                    libusbi
+LOCAL_MODULE := libusb_lib
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
