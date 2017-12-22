1713daf53cbe84f0da85bb15c352936ffec6923e
sbouaala <samah-rim.bouaalam@utbm.fr>
2017-12-22 12:33:17 +0100

    Creation of the NDK project

---
+++ Mages/TP5/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,11 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_tp5_tp5_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
