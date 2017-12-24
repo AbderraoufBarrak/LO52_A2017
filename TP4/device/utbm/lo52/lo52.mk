c38cd692c90cfd045d3cc9d5e228db58e0c67a90
LO52_Comateux_Numérique <vavaburger@gmail.com>
2017-12-24 18:01:11 +0100

    TP4-Part2

---
+++ TP4/device/utbm/lo52/lo52.mk
@@ -0,0 +1,15 @@
+$(call inherit-product,device/linaro/hikey/Android.mk)
+
+PRODUCT_PACKAGES+= \
+		libusb
+DEVICE_PACKAGE_OVERLAYS:= device/utbm/lo52/overlay
+PRODUCT_PROPERTY_OVERRIDES+= \
+		ro.hw=lo52 \
+		net.dns1=8.8.8.8 \
+		net.dns2=4.4.4.4
+PRODUCT_NAME:= lo52
+PRODUCT_DEVICE:= lo52
+PRODUCT_BRAND:= Android
+PRODUCT_MODEL:= LO52_UTBM
+
+include $(call all-subdirs-makefiles)
